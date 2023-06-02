44da7fbabe1ec3d3e39da13f0c695a76848452c9
AlanLeonelMaciel <103143446+AlanLeonelMaciel@users.noreply.github.com>
2023-06-01 23:39:49 -0300

    Add files via upload

---
+++ guia7-ext1/guia7-ext1/src/guia7/ext1/Guia7Ext1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia7.ext1;
+
+import entidad.cancion;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Guia7Ext1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        cancion c1=new cancion();
+        c1.crearCancion();
+        System.out.println(c1);
+        
+    }
+    
+}
